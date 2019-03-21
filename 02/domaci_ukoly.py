8825d52628d0e9d3e9e26581b4ca310debe97df4
evajs <evajs@matfyz.cz>
2019-03-21 22:12:45 +0100

    domaci ukol 2

---
+++ 02/domaci_ukoly.py
@@ -0,0 +1,55 @@
+################### ukol 1 #####################
+#
+# hrana=2852
+# print('Povrch krychle o hraně', hrana, 'je', 6*hrana**2, 'centimetrů čtverečných.')
+# print('Objem krychle o hraně', hrana, 'je', hrana**3, 'centimetrů krychlových.')
+
+################### ukol 2 #####################
+#
+# hrana= float(input('Zadej hranu krychle v centimetrech'))
+# print('Povrch krychle o hraně ', hrana, ' je ', 6*hrana**2, '.')
+# print('Objem krychle o hraně', hrana, ' je ', hrana**3, '.')
+
+################### ukol 3 #####################
+# from random import randrange
+# cislo = randrange(3)
+# print(cislo)
+
+# Vrací náhodně nulu, jedničku nebo dvojku.
+
+################### ukol 4 #####################
+# heslo = 'zmrzlina'
+# zadano = input('Zadej heslo:')
+# if zadano == heslo:
+#     print('Pšššt. Všechny děti spí.')
+# else:
+#     print('Nesprávné heslo. Žádné tajemnství se nedozvíš.')
+
+################### ukol 4 #####################
+# verzi 3.7
+# PyCharm > Preferences > Project:PyLadies > Project Interpreter
+
+################### ukol 5 #####################
+# Dáš uvozovky jen na jednu stranu řetězce? — SyntaxError
+# Zkusíš odečíst číslo od řetězce? — TypeError
+# Dělíš nulou? — ZeroDivisionError
+# Použiješ proměnnou, která neexistuje? — NameError
+# Stiskneš Ctrl+C, když se program ptá na vstup (pomocí input)? — InputError
+# Odsadíš příkaz bez předchozího if:? — IndentationError
+# Po if: odsadíš jeden příkaz o čtyři mezery a druhý jen o dvě? — IndentationError
+# Neuzavřeš závorku? – SyntaxError
+# Zkusíš použít vykřičník (!) jako operátor? — SyntaxError
+# Napíšeš v příkazu print(1, 2, 3) čárku navíc? — SyntaxError
+
+################### ukol 6 #####################
+# NameError
+
+################### ukol 7 #####################
+# TypeError
+
+################### ukol 8 #####################
+# když operátor nelze použít pro daný typ (lomítko), vrací TypeError
+# když operátor vůbec neexistuje, vrací SyntaxError
+
+################### ukol 9 #####################
+
