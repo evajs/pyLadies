b7efbb9107159e8d16d3408f4311e6f0efa411d1
evajs <evajs@matfyz.cz>
2019-03-26 11:47:51 +0100

    03 ukoly 4-7

--- 03/domaci_ukoly.py
+++ 03/domaci_ukoly.py
@@ -1,14 +1,13 @@
-################ ukol 1 ################
+################ ukol 0 ################
 # seznam cisle od 0 do n-1
 
-################ ukol 2 ################
+################ ukol 1 ################
 # from turtle import forward, left, exitonclick, penup, pendown
 #
 # for n in range(5,9):
 #     for cisloStrany in range(n):
-#         delkaStrany= 200/n
-#         uhel = 360/n
-#         print(uhel)
+#         delkaStrany= 200.0/n
+#         uhel = 360.0/n
 #         forward(delkaStrany)
 #         left(uhel)
 #     penup()
@@ -17,16 +16,72 @@
 #
 # exitonclick()
 
+################ ukol 2 ################
+# from turtle import forward, left, exitonclick, penup, pendown
+#
+# pocet = int(input('Zadej počet stran n-úhelníku:'))
+# delkaStrany = 200.0/pocet
+# uhel = 360.0/pocet
+#
+# for cisloStrany in range(pocet):
+#     forward(delkaStrany)
+#     left(uhel)
+#
+# exitonclick()
+
 ################ ukol 3 ################
-from turtle import forward, left, exitonclick, penup, pendown
+# cislo1 = input('První číslo:')
+# cislo2 = input('Druhé číslo:')
+# operace =input('Operace:')
+#
+# if operace =='+':
+#     print(cislo1+operace+cislo2, '=', float(cislo1)+float(cislo2))
+# elif operace =='-':
+#     print(cislo1 + operace + cislo2, '=', float(cislo1) - float(cislo2))
+# elif operace =='*':
+#     print(cislo1 + operace + cislo2, '=', float(cislo1) * float(cislo2))
+# elif operace =='/':
+#     print(cislo1 + operace + cislo2, '=', float(cislo1) / float(cislo2))
+# else:
+#     print('Neznámá operace')
 
-pocet = int(input('Zadej pocet stran n-uhelniku:'))
-delkaStrany = 200/pocet
-uhel = 360/pocet
-print(delkaStrany, uhel)
+################ ukol 4 ################
+# range(m, n) vraci seznam hodnot od m do n-1
 
-for cisloStrany in range(pocet):
-    forward(delkaStrany)
-    left(uhel)
+################ ukol 5 ################
+# range(m, n, k) vraci seznam hodnot od m s krokem k do posledni hodnoty mensi nez n
 
-exitonclick()
+################ ukol 6 ################
+# from turtle import forward, left, exitonclick, penup, pendown
+#
+# pocet = 3
+# delkaStrany = 200.0/pocet
+# uhel = 360.0/pocet
+#
+# for cisloStrany in range(pocet):
+#     forward(delkaStrany)
+#     left(uhel)
+#
+# exitonclick()
+
+################ ukol 7 ################
+# from turtle import forward, left, right, exitonclick, penup, pendown
+# from math import sqrt
+#
+# uhlopricka = 50 * sqrt(2)
+#
+# for cisloStrany in range(4):
+#     forward(50)
+#     left(90)
+#
+# left(45)
+# forward(uhlopricka)
+# left(90)
+# forward(uhlopricka/2)
+# left(90)
+# forward(uhlopricka/2)
+# left(90)
+# forward(uhlopricka)
+# left(45)
+#
+# exitonclick()
