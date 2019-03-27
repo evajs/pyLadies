a7fc180d44d4f5a2aa1d02f4072c80dcc2ccfc18
evajs <evajs@matfyz.cz>
2019-03-27 11:16:19 +0100

    03 ukoly 1-13, 17

--- 03/domaci_ukoly.py
+++ 03/domaci_ukoly.py
@@ -85,3 +85,105 @@
 # left(45)
 #
 # exitonclick()
+
+
+################ ukol 8 ################
+# from turtle import forward, left, right, exitonclick, penup, pendown
+# from math import sqrt
+#
+# uhlopricka = 50 * sqrt(2)
+#
+# for cisloDomu in range(10):
+#     for cisloStrany in range(4):
+#         forward(50)
+#         left(90)
+#     left(45)
+#     forward(uhlopricka)
+#     left(90)
+#     forward(uhlopricka/2)
+#     left(90)
+#     forward(uhlopricka/2)
+#     left(90)
+#     forward(uhlopricka)
+#     left(45)
+#     penup()
+#     forward(10)
+#     pendown()
+#
+# exitonclick()
+
+################ ukol 9 ################
+# from turtle import forward, left, exitonclick, penup, pendown
+#
+# pocet = 200
+# delkaStrany = 200.0/pocet
+# uhel = 360.0/pocet
+#
+# for cisloStrany in range(pocet):
+#     forward(delkaStrany)
+#     left(uhel)
+#
+# exitonclick()
+
+# ################ ukol 10 ################
+# from turtle import forward, left, exitonclick, penup, pendown
+#
+# pocet = 95
+# delkaStrany = 200.0/pocet
+# uhel = 360.0/pocet
+#
+# for cisloStrany in range(pocet):
+#     forward(delkaStrany)
+#     left(uhel)
+#
+# exitonclick()
+
+################ ukol 11 ################
+# from turtle import forward, left, right, exitonclick, penup, pendown
+#
+# delkaStrany = 90
+# left(90)
+# for cisloStrany in range(19):
+#     forward(delkaStrany)
+#     right(90)
+#     delkaStrany = delkaStrany - 5
+#
+# exitonclick()
+
+################ ukol 12 ################
+# from turtle import forward, left, right, exitonclick, penup, pendown
+#
+# delkaStrany = 118
+# uhel=360.0/8
+# right(uhel)
+# for cisloStrany in range(58):
+#     forward(delkaStrany)
+#     right(uhel)
+#     delkaStrany = delkaStrany - 2
+#
+# exitonclick()
+
+################ ukol 13 ################
+# TODO
+# from turtle import forward, left, right, exitonclick, penup, pendown
+#
+# pocetStranUhelniku =100
+# pocetStranSpiraly =int(16.5*pocetStranUhelniku)
+# delkaStrany = 5
+# uhel=360.0/pocetStranUhelniku
+# left(45)
+# for cisloStranySpiraly in range(pocetStranSpiraly):
+#     forward(delkaStrany)
+#     right(uhel)
+#     delkaStrany = delkaStrany - delkaStrany/pocetStranSpiraly
+#
+# exitonclick()
+
+################ ukol 17 ################
+# vysledek = input('Zadej číslo:')
+#
+# for opakovani in range(4):
+#     cislo=input('Zadej číslo:')
+#     vysledek = min(vysledek, cislo)
+#
+# print('Nejmenší číslo bylo:', vysledek, end='.\n')
