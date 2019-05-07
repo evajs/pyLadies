0850199d1618a2182abb2535cb80aa89ee15e3df
evajs <evajs@matfyz.cz>
2019-05-07 11:54:30 +0200

    domaci ukol piskvorky, predelani na testovatelnou verzi

--- 09/hra.py
+++ 09/hra.py
@@ -1,6 +1,28 @@
 # hra.py
 import piskvorky
 import ai
+import util
+
+# def piskvorky1d():
+#     pole='--------------------'
+#     stav_hry = '-'
+#     hraje = 1
+#     while stav_hry == '-':
+#         if hraje==1:
+#             pole = piskvorky.tah_hrace(pole)
+#             print(pole)
+#             hraje =2
+#         else:
+#             pole = ai.tah_pocitace(pole)
+#             print(pole)
+#             hraje =1
+#         stav_hry = piskvorky.vyhodnot(pole)
+#     if stav_hry =='x':
+#         print('Vyhrál jsi. Gratulujeme.')
+#     elif stav_hry =='o':
+#         print('Vyhrál počítač.')
+#     elif stav_hry == '!':
+#         print('Remíza.')
 
 def piskvorky1d():
     pole='--------------------'
@@ -8,7 +30,13 @@ def piskvorky1d():
     hraje = 1
     while stav_hry == '-':
         if hraje==1:
-            pole = piskvorky.tah_hrace(pole)
+            novy_vstup = True
+            while novy_vstup:
+                vstup = int(input('Zadej číslo pole, kam chceš hrát (0-19): '))
+                hlaska, novy_vstup = piskvorky.(vstup)
+                if novy_vstup:
+                    print(hlaska)
+            pole = util.tah(pole,vstup,'x')
             print(pole)
             hraje =2
         else:
@@ -22,5 +50,8 @@ def piskvorky1d():
         print('Vyhrál počítač.')
     elif stav_hry == '!':
         print('Remíza.')
+
+
+
 
 piskvorky1d()
--- 09/piskvorky.py
+++ 09/piskvorky.py
@@ -10,15 +10,16 @@ import util
 #         cislo_policka = int(input('Zadej číslo pole, kam chceš hrát (0-19): '))
 #     return util.tah(pole,cislo_policka,'x')
 
-def tah_hrace(pole, vstup):
+def over_vstup(pole, vstup):
     novy_vstup = False
     if vstup<0 or vstup>=20:
         novy_vstup = True
-        return '{} není v rozsahu hracího pole. Zadej číslo od 0 do 19.'.format(vstup),novy_vstup
-    if pole[cislo_policka]!='-':
-        print('Pole {} je obsazené'.format(cislo_policka))
-        cislo_policka = int(input('Zadej číslo pole, kam chceš hrát (0-19): '))
-    return util.tah(pole,cislo_policka,'x')
+        return '{} není v rozsahu hracího pole. Zadej číslo od 0 do 19.'.format(vstup), novy_vstup
+    elif pole[vstup]!='-':
+        novy_vstup = True
+        return 'Pole {} je obsazené.'.format(vstup), novy_vstup
+    else:
+        return '{} je dostupný tah.'.format(vstup), novy_vstup
 
 def vyhodnot(pole):
     konec = True
