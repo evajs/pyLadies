2e30951a4cd46d97f44f2e380c6c6c2332e7fbb6
evajs <evajs@matfyz.cz>
2019-05-09 12:09:10 +0200

    predelani piskvorek podle review

--- 09/hra.py
+++ 09/hra.py
@@ -34,11 +34,11 @@ def piskvorky1d():
                 hlaska, novy_vstup = piskvorky.over_vstup(pole,vstup)
                 if novy_vstup:
                     print(hlaska)
-            pole = piskvorky.tah(pole,vstup,'x')
+            pole = piskvorky.util.tah(pole,vstup,'x')
             print(pole)
             hraje =2
         else:
-            pole = piskvorky.tah_pocitace(pole)
+            pole = piskvorky.ai.tah_pocitace(pole)
             print(pole)
             hraje =1
         stav_hry = piskvorky.vyhodnot(pole)
--- 09/piskvorky.py
+++ 09/piskvorky.py
@@ -1,12 +1,13 @@
 import ai
+import util
 
 # def tah_hrace(pole):
 #     cislo_policka = int(input('Zadej číslo pole, kam chceš hrát (0-19): '))
-#     while cislo_policka<0 or cislo_policka>=20:
-#         print('{} není v rozsahu hracího pole. Zadej číslo od 0 do 19.'.format(cislo_policka))
-#         cislo_policka = int(input('Zadej číslo pole, kam chceš hrát (0-19): '))
-#     while pole[cislo_policka]!='-':
-#         print('Pole {} je obsazené'.format(cislo_policka))
+#     while cislo_policka<0 or cislo_policka>=20 or pole[cislo_policka]!='-':
+#         if cislo_policka<0 or cislo_policka>=20:
+#             print('{} není v rozsahu hracího pole. Zadej číslo od 0 do 19.'.format(cislo_policka))
+#         elif pole[cislo_policka]!='-':
+#             print('Pole {} je obsazené'.format(cislo_policka))
 #         cislo_policka = int(input('Zadej číslo pole, kam chceš hrát (0-19): '))
 #     return util.tah(pole,cislo_policka,'x')
 
@@ -21,29 +22,41 @@ def over_vstup(pole, vstup):
     else:
         return '{} je dostupný tah.'.format(vstup), novy_vstup
 
+# def vyhodnot(pole):
+#     konec = True
+#     kolecka = 0
+#     krizky = 0
+#     for znak in pole:
+#         if znak=='-':
+#             konec = False
+#             kolecka = 0
+#             krizky = 0
+#         elif znak == 'x':
+#             krizky = krizky+1
+#             if krizky>=3:
+#                 return 'x'
+#             kolecka = 0
+#         elif znak =='o':
+#             kolecka = kolecka+1
+#             if kolecka >= 3:
+#                 return 'o'
+#             krizky = 0
+#     if konec:
+#         return '!'
+#     else:
+#         return '-'
+
 def vyhodnot(pole):
-    konec = True
-    kolecka = 0
-    krizky = 0
-    for znak in pole:
-        if znak=='-':
-            konec = False
-            kolecka = 0
-            krizky = 0
-        elif znak == 'x':
-            krizky = krizky+1
-            if krizky>=3:
-                return 'x'
-            kolecka = 0
-        elif znak =='o':
-            kolecka = kolecka+1
-            if kolecka >= 3:
-                return 'o'
-            krizky = 0
-    if konec:
-        return '!'
-    else:
+    if 'xxx' in pole:
+        return 'x'
+    elif 'ooo' in pole:
+        return 'o'
+    elif '-' in pole:
         return '-'
+    else:
+        return '!'
+
+# print(vyhodnot('xxooxxooxxoxxooxxoo-'))
 
 def jak_to_dopadlo(stav_hry):
     if stav_hry =='x':
